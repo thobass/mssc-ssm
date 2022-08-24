91fdd8829f3afcbb28cbd3e3b2f9a9283bdd6689
BASSET, Thomas <thomas.basset@cgi.com>
2022-08-24 21:50:27 +0200

    Add transition configuration

---
+++ src/test/java/guru/springframework/msscssm/config/StateMachineConfigTest.java
@@ -0,0 +1,34 @@
+package guru.springframework.msscssm.config;
+
+import guru.springframework.msscssm.domain.PaymentEvent;
+import guru.springframework.msscssm.domain.PaymentState;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+
+import java.util.UUID;
+
+@SpringBootTest
+class StateMachineConfigTest {
+
+    @Autowired
+    StateMachineFactory<PaymentState, PaymentEvent> factory;
+
+    @Test
+    void testNewStateMachine() {
+        StateMachine<PaymentState, PaymentEvent> stateMachine = factory.getStateMachine(UUID.randomUUID());
+        stateMachine.start();
+
+        System.out.println(stateMachine.getState().toString());
+
+        stateMachine.sendEvent(PaymentEvent.PRE_AUTHORIZE);
+
+        System.out.println(stateMachine.getState().toString());
+
+        stateMachine.sendEvent(PaymentEvent.PRE_AUTH_APPROVED);
+
+        System.out.println(stateMachine.getState().toString());
+    }
+}
